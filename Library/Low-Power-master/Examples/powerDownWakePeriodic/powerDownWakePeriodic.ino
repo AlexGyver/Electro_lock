d63438e40239541ad6d25f30813829cf8a9c96b2
Alex <beragumbo@ya.ru>
2017-08-27 16:16:29 +0300

    обновлено

--- Library/Low-Power-master/Examples/powerDownWakePeriodic/powerDownWakePeriodic.ino
+++ Library/Low-Power-master/Examples/powerDownWakePeriodic/powerDownWakePeriodic.ino
@@ -1,16 +1,10 @@
-// **** INCLUDES *****
 #include "LowPower.h"
 
-void setup()
-{
-    // No setup is required for this library
+void setup() {
+
 }
 
-void loop() 
-{
-    // Enter power down state for 8 s with ADC and BOD module disabled
-    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);  
-    
-    // Do something here
-    // Example: Read sensor, data logging, data transmission.
+void loop() {
+  delay(4000);
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
 }
